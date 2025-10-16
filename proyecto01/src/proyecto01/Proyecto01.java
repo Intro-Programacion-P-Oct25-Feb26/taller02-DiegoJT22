95d5b5eaa20f69c5c659a25600e7bbf9492a2faa
DiegoJT22 <daigoestrlar22@gmail.com>
2025-10-16 11:34:22 -0500

    segundo commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -15,6 +15,7 @@ public class Proyecto01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Ejemplo basico de Java Versionado a traves de Git Manejado desde Git Creado por DiegoJT22");
     }
     
 }
